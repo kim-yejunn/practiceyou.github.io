ca67ee37303ca8183e293a4003f97cda47e65ca4
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2024-09-27 20:42:02 +0900

    Update app.py
    파일 업로드를 처리하는 / 경로를 추가하여 업로드된 파일을 저장하고 필터링

--- app.py
+++ app.py
@@ -127,22 +127,36 @@ def create_app():
      # 세션을 사용하기 위한 비밀 키 설정
     app.secret_key = 'aa8pepole'  # 여기에 고유하고 비밀스러운 문자열을 넣으세요
 
-    # 블루프린트 정의 및 등록을 create_app 함수 안에서 처리
-    bp = Blueprint('fileupload', __name__, url_prefix='/fileupload')
+@bp.route('/', methods=['GET', 'POST'])
+def upload_file():
+    name = session.get('name')
 
-    @bp.route('/result')
-    def result():
-        name = session.get('name')  # 세션에서 이름 가져오기
-        return render_template('result.html', name=name)  # result.html에 이름 전달
+    if os.path.exists(Upload_Folder):
+        for file in os.scandir(Upload_Folder):
+            os.remove(file.path)
 
-    # 블루프린트를 앱에 등록
-    app.register_blueprint(bp)
+    if request.method == 'POST':
+        files = request.files.getlist('savefile')
+        for file in files:
+            filename = file.filename
+            filepathtosave = os.path.join(Upload_Folder, filename)
+            file.save(filepathtosave)
+        
+        merge_file_path = extraction()
+        filter_chat(merge_file_path, name)
+        
+        return redirect(url_for('fileupload.result'))
 
-    @app.route('/')
-    def home():
-        return render_template('index.html')
+    return render_template('submit.html', name=name)
 
-    return app
+# 블루프린트를 앱에 등록
+app.register_blueprint(bp)
+
+@bp.route('/result')
+def result():
+    name = session.get('name')
+    return render_template('result.html', name=name)
+
 
 # 앱 실행
 if __name__ == '__main__':
